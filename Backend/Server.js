e68bebc85b563aed47d90984d576336655a507c2
salouaEng <saloua.ouissa@edu.uiz.ac.ma>
2025-02-10 17:30:04 +0100

    create formation

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const dotenv = require('dotenv');
  const connectDB = require('./Config/config.js');
 const Auth = require('./Routes/auth.js');
-// const cookieParser = require('cookie-parser');
+const formateurRoute = require('./Routes/formateur.route.js');
 
+const cookieParser = require('cookie-parser');
 dotenv.config();
 const app = express();
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 5000;
 console.log()
 // Middleware --
@@ -23,7 +26,8 @@ app.use(
 // Connect to the database
   
 
-app.use('/api/auth', Auth);
+app.use('/api/auth',Auth);
+app.use('/api/formateur',formateurRoute);
 
 
 
