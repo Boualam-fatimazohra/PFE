398d6b25a5d6f532e9b5c7f88f5667d72030e781
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-11 14:33:22 +0100

    update certification model

--- Backend/Models/certification.model.js
+++ Backend/Models/certification.model.js
@@ -3,11 +3,23 @@ const mongoose = require('mongoose');
 const certificationSchema = new mongoose.Schema({
     formation: { 
         type: mongoose.Schema.Types.ObjectId,
-         ref: "Formation",
-          required: true },
-    beneficiaires:
-     [{ type: mongoose.Schema.Types.ObjectId,
-         ref: "Beneficiaire" }]
-  }, { timestamps: true });
-  export const Certification = mongoose.model("Certification", certificationSchema);
-  
+        ref: "Formation",
+        required: true 
+    },
+    beneficiaires: [
+        { 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Beneficiaire",
+            validate: {
+                validator: function(value) {
+                    return Array.isArray(value) && value.every(item => mongoose.Types.ObjectId.isValid(item));
+                },
+                message: 'Each beneficiaire must be a valid ObjectId reference.'
+            }
+        }
+    ]
+}, { timestamps: true });
+
+const Certification = mongoose.model("Certification", certificationSchema);
+
+module.exports = Certification;
