6ad95f19a4dedce015731a220ccbce12d247865a
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-11 12:19:27 +0100

    Default value for formations to an empty array if no formations

--- Backend/Models/formateur.model.js
+++ Backend/Models/formateur.model.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 const formateurSchema = new mongoose.Schema({
-    utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true },
-    formations: [{ type: mongoose.Schema.Types.ObjectId, ref: "Formation" }]
-  }, { timestamps: true });
-  const Formateur = mongoose.model("Formateur", formateurSchema);
-  
+    utilisateur: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Utilisateur", 
+        required: true 
+    },
+    formations: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Formation", 
+        default: [] // default to an empty array if no formations
+    }]
+}, { timestamps: true });
+
+const Formateur = mongoose.model("Formateur", formateurSchema);
+
+module.exports = { Formateur };
