8d80221b15cdab6338bcf4550adc2dd197801c65
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-11 14:30:40 +0100

    update classe model

--- Backend/Models/classe.model.js
+++ Backend/Models/classe.model.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const classeSchema = new mongoose.Schema({
-    formations: [{ type: mongoose.Schema.Types.ObjectId, ref: "Formation" }]
-  }, { timestamps: true });
-  const Classe = mongoose.model("Classe", classeSchema);
-  
+    formation: { type: mongoose.Schema.Types.ObjectId, ref: "Formation", required: true },  // Link to a single formation
+    formateur: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true },  // The teacher (formateur) of the class
+}, { timestamps: true });
+
+const Classe = mongoose.model("Classe", classeSchema);
+
+module.exports = Classe;
