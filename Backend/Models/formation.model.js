64b6c220bcbf30af5aa82e27f889b8f6e5fa76c3
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-11 12:16:37 +0100

    -Validation for dateFin o ensure dateFin is after dateDebut -Default value for tauxSatisfaction

--- Backend/Models/formation.model.js
+++ Backend/Models/formation.model.js
@@ -3,10 +3,20 @@ const mongoose = require('mongoose');
 const formationSchema = new mongoose.Schema({
   nom: { type: String, required: true },
   dateDebut: { type: Date, required: true },
-  dateFin: { type: Date, required: true },
+  dateFin: { 
+    type: Date, 
+    required: true,
+    validate: {
+      validator: function(value) {
+        // Ensure dateFin is after dateDebut
+        return value > this.dateDebut;
+      },
+      message: 'dateFin must be after dateDebut'
+    }
+  },
   lienInscription: { type: String },
   tags: { type: String, required: true },
-  tauxSatisfaction: { type: Number, min: 0, max: 100,required: false },
+  tauxSatisfaction: { type: Number, min: 0, max: 100, required: false, default: null }, // optional with default null
   formateur: { type: mongoose.Schema.Types.ObjectId, ref: "Formateur", required: true },
   classes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Classe" }]
 }, { timestamps: true });
