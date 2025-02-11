f56d2b1f139a6d2b5410be5147ba92214496d971
AS-AADIkram <ikramasaad2002@gmail.com>
2025-02-11 16:57:51 +0100

    fix:fixing_header_footer

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-
 import ForgotPassword from "./pages/ForgotPassword";
 import NotFound from "./pages/NotFound";
 import FormateurRoutes from "./routes/FormateurRoutes";
@@ -16,6 +15,9 @@ import FormulaireEvaluation from "./components/dashboardElement/FormulaireEvalua
 
 
 import { ToastContainer } from "react-toastify";
+import { DashboardHeader } from "@/components/layout/DashboardHeader";
+import { Footer } from "@/components/layout/Footer";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -25,23 +27,33 @@ const App = () => (
       <ToastContainer />
       <Sonner />
       <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          
-          {/* Route parent pour les formateurs */}
-          <Route path="/formateur/*" element={<FormateurRoutes />} />
-          <Route path="/manager/*" element={<ManagerRoutes />} />
-          <Route path="/coordinateur/*" element={<CoordinateurRoutes />} />
-          <Route path="/Technicien/*" element={<TechnicienRoutes />} />
+        <div className="pt-[70px] pb-[60px] min-h-screen">
+          {/* Header */}
+          <DashboardHeader />
 
-          <Route path="*" element={<NotFound />} />
+          {/* Main content */}
           
-        <Route path="/" element={<GenerateLink  />} />
-        <Route path="/formulaire-evaluation" element={<FormulaireEvaluation />} /> 
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              
+              {/* Routes pour les différents types d'utilisateurs */}
+              <Route path="/formateur/*" element={<FormateurRoutes />} />
+              <Route path="/manager/*" element={<ManagerRoutes />} />
+              <Route path="/coordinateur/*" element={<CoordinateurRoutes />} />
+              <Route path="/Technicien/*" element={<TechnicienRoutes />} />
+              
+              <Route path="*" element={<NotFound />} />
+           
+          
+            <Route path="/" element={<GenerateLink  />} />
+            <Route path="/formulaire-evaluation" element={<FormulaireEvaluation />} /> 
     
         </Routes>
 
+          {/* Footer */}
+          <Footer />
+        </div>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
