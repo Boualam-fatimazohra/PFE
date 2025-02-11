0c035c4aa06aec1e5874c87b50f9e3c1db7dd007
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-11 12:04:03 +0100

    "Updated manager schema to allow multiple coordinateurs per manager"

--- Backend/Models/manager.model.js
+++ Backend/Models/manager.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const managerSchema = new mongoose.Schema({
     utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true },
     formateurs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur" }],
-    coordinateur: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true }
+    coordinateurs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true }]
   }, { timestamps: true });
   const Manager = mongoose.model("Manager", managerSchema);
   
