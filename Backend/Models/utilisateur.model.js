3db538e77fd146fc9324b25764c4436b52e9e70a
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-11 11:45:57 +0100

    efactor Utilisateur model: Added 'required' validations for firstName, lastName, phoneNumber, and email fields.

--- Backend/Models/utilisateur.model.js
+++ Backend/Models/utilisateur.model.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
-/**
- * Schéma Utilisateur (hérite pour Manager, Formateur, Coordinateur)
- */
+
 const utilisateurSchema = new mongoose.Schema({
-    firstName: { type: String },
-    lastName: { type: String },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phoneNumber: { type: String },
+    phoneNumber: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["Manager", "Formateur", "Coordinateur"], required: true }
+    role: { 
+        type: String, 
+        enum: ["Manager", "Formateur", "Coordinateur"], 
+        required: true 
+    }
 }, { timestamps: true });
+
 const Utilisateur = mongoose.model("Utilisateur", utilisateurSchema);
-module.exports = {Utilisateur};
+
+module.exports = Utilisateur;
