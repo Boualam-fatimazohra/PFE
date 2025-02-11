c723f769de67df8f2310ed5b0d7e4ba36845daa9
salouaEng <saloua.ouissa@edu.uiz.ac.ma>
2025-02-11 12:30:25 +0100

    commit basic formation

--- Backend/Models/formateur.model.js
+++ Backend/Models/formateur.model.js
@@ -4,5 +4,5 @@ const formateurSchema = new mongoose.Schema({
     utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true },
     formations: [{ type: mongoose.Schema.Types.ObjectId, ref: "Formation" }]
   }, { timestamps: true });
-  const Formateur = mongoose.model("Formateur", formateurSchema);
-  
+ const Formateur = mongoose.model("Formateur", formateurSchema);
+ module.exports = {Formateur};
