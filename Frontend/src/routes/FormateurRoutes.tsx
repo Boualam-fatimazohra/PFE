71e592a0d4a9ed595348bdc1fd88410ea5177840
AS-AADIkram <ikramasaad2002@gmail.com>
2025-02-12 17:00:39 +0100

    feat:page_mesformation

--- Frontend/src/routes/FormateurRoutes.tsx
+++ Frontend/src/routes/FormateurRoutes.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import DashboardFormateur from "../pages/DashboardFormateur";
 import Dashboard from "../pages/DashboardFormateur";
 import NotFound from "../pages/NotFound";
+import MesFormations from "@/pages/MesFormation";
 
 const FormateurRoutes = () => {
   return (
     <Routes>
       <Route path="dashboardFormateur" element={<DashboardFormateur />} />
+      <Route path="mesformation" element={<MesFormations />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
