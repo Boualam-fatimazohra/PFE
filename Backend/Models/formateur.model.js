e0cb8a5f8dce239ec11215bc47befbff3ca53260
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-13 15:45:27 +0100

    add to formateur logic (model - controller )

--- Backend/Models/formateur.model.js
+++ Backend/Models/formateur.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const formateurSchema = new mongoose.Schema({
     utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true },
     //todo : changer required from false to true 
-    manager: { type: mongoose.Schema.Types.ObjectId, ref: "Manager", required: false },
+    manager: { type: mongoose.Schema.Types.ObjectId, ref: "Manager", required: true },
+    coordinateur: { type: mongoose.Schema.Types.ObjectId, ref: "Coordinateur", required: true },
   }, { timestamps: true });
  const Formateur = mongoose.model("Formateur", formateurSchema);
 
