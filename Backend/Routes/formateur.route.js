354f9f49bbb4de2dcfc0687b7ba94c84a2c817be
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-17 10:05:34 +0100

    udd role middleware and set example in formateur and formation route

--- Backend/Routes/formateur.route.js
+++ Backend/Routes/formateur.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { createFormateur, getFormateurs,updateFormateur,deleteFormateur, getFormateurById } = require('../Controllers/formateur.controller.js');
-
+const authorizeRoles = require('../Middlewares/RoleMiddleware.js');
 
 const router = express.Router();
 
@@ -10,3 +10,11 @@ router.put('/updateFormateur/:id',updateFormateur);
 router.delete('/deleteFormateur/:id',deleteFormateur);
 router.get('/getFormateurById/:id',getFormateurById);
 module.exports = router;    
+
+
+/*
+// allowed roles example
+router.get('/getFormateurById/:id', authorizeRoles('Admin', 'Manager', 'Formateur'), updateFOrmateur); 
+// Everyone can see their own profile
+*/
+
