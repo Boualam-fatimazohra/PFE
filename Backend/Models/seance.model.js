198a2cda1a047158a372373e7bd77dcbe3624e39
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-17 09:38:47 +0100

    update seance

--- Backend/Models/seance.model.js
+++ Backend/Models/seance.model.js
@@ -1,10 +1,5 @@
 const seanceSchema = new mongoose.Schema({
     formation: { type: mongoose.Schema.Types.ObjectId, ref: "Formation", required: true },
-    beneficiaire: { type: mongoose.Schema.Types.ObjectId, ref: "Beneficiaire", required: true },
-    presence :[
-      {date: { type: Date, required: true },
-      present: { type: Boolean, default: false }}// Indique si le bénéficiaire était présent
-    ]
   }, { timestamps: true });
   
   const Seance = mongoose.model("Seance", seanceSchema);
