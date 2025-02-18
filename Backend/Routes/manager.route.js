67a612dffc707615c5f3dd8cf0753e1cd4ebc3fe
2tay <hakim.toutay@e-polytechnique.ma>
2025-02-18 16:15:34 +0100

    Ownership Middleware where checking privilege (manager-formateur) who can access some endpoints - adapt manager route and formateur route to this middleware

--- Backend/Routes/manager.route.js
+++ Backend/Routes/manager.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authenticated = require('../Middlewares/Authmiddleware.js');
 const authoriezeRoles = require('../Middlewares/RoleMiddleware.js');
+const authorizeSelfGetUpdate = require('../Middlewares/selfAccess.js');
 const {
     createManager,
     getManagers,
@@ -15,8 +16,8 @@ const {
 router.post("/",authenticated,authoriezeRoles('Admin'), createManager);
 router.get("/",authenticated,authoriezeRoles('Admin'), getManagers);
 // todo : il faut ajouter un middelware pour vérifier est ce que l'id de manager dans req et le meme que l'id passé en paramétre 
-router.get("/:id",authenticated,authoriezeRoles('Admin',"Manager"),getManagerById);
-router.put("/:id",authenticated,authoriezeRoles('Admin',"Manager"), updateManager);
-router.delete("/:id",authenticated,authoriezeRoles('Admin',"Manager"), deleteManager);
+router.get("/:id",authenticated,authoriezeRoles('Admin',"Manager"), authorizeSelfGetUpdate, getManagerById);
+router.put("/:id",authenticated,authoriezeRoles('Admin',"Manager"), authorizeSelfGetUpdate, updateManager);
+router.delete("/:id",authenticated,authoriezeRoles('Admin'), deleteManager);
 
 module.exports = router;
