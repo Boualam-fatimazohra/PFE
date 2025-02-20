923971589d76a5d8e9ea4600a201f1fa540b6fc8
salouaEng <saloua.ouissa@edu.uiz.ac.ma>
2025-02-20 10:33:17 +0100

    test

--- Backend/Routes/formation.route.js
+++ Backend/Routes/formation.route.js
@@ -3,7 +3,7 @@ const authenticated = require('../Middlewares/Authmiddleware.js');
 const { createFormation, GetOneFormation, UpdateFormation, GetFormations, DeleteFormation, GetFormationOfMentor } = require('../Controllers/formation.controller.js'); // Removed duplicate DeleteFormation
 const authorizeRoles = require('../Middlewares/RoleMiddleware.js');
 const authorizeSelfGetUpdate = require('../Middlewares/selfAccess.js');
-const upload = require('../utils/upload');
+const upload = require('../utils/uploadImage');
 const router = express.Router();
 
 // Route to add a new formation (Protected route: Only authenticated users can access)
@@ -11,16 +11,12 @@ router.post('/Addformation', authenticated, authorizeRoles('Formateur'),upload.s
 
 // Route to get all formations (No authentication required)
 router.get('/GetFormations', authenticated, GetFormations);
-
 // Route to delete a formation by ID
 router.delete('/DeleteFormation/:id', authenticated, authorizeRoles('Admin', 'Manager'), DeleteFormation);
-
 // Route to update a formation by ID 
 router.put('/UpdateFormation/:id', authenticated, authorizeRoles('Admin', 'Manager'), UpdateFormation); 
-
 // Route to get one specific formation by ID
 router.get('/GetOneFormation/:id',authenticated, GetOneFormation);
-
 router.get('/GetFormationOfMentor', authenticated, GetFormationOfMentor);
 
 module.exports = router;
