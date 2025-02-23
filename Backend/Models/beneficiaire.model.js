cf2f472aa92a8508ab656953c26936853f63c29c
salouaEng <saloua.ouissa@edu.uiz.ac.ma>
2025-02-23 23:52:24 +0100

    modification de createFormateur & fct qui getFormateurByManager & ajout de qlq att dans beneficiaire

--- Backend/Models/beneficiaire.model.js
+++ Backend/Models/beneficiaire.model.js
@@ -7,16 +7,21 @@ const beneficiaireSchema = new mongoose.Schema({
     dateNaissance: { type: Date, required:false},
     email:{type:String,required:true},
     genre :{type:String,required:true},
+    telephone:{type:Number,required : false},
     pays:{type:String,required:true},
-    //,enum :["bac","bac+2","bac+3","bac+5"]
-    niveau: { type: String, required:false },
+    //enum :["bac","bac+2","bac+3","bac+5"]
+    niveau: { type: String, required:false,enum :["bac","bac+2","bac+3","bac+5"]},
     specialite: {type:String,required:false},
     etablissement:{type:String,required:false},
-    // ,enum:["Sans Emploi","Avec Emploi","Etudiant"]
+    //enum:["Sans Emploi","Avec Emploi","Etudiant"]
     profession:{type:String,required:false},
+    situationProfessionnel:{type:String,required:false,enum:["Sans Emploi","Avec Emploi","Etudiant"]},
+    // est ce que le beneficiaire est blacklister ou pas 
     isBlack: { type: Boolean, default: false },
+    // est ce qu'il a dépasser 3 formation ou pas 
     isSaturate: { type: Boolean, default: false },
     nationalite: {type:String,required:false},
+    region:{type:String,required:false,enum:["Souss Massa","Draa Tafilalte"],default:"null"}
   }, { timestamps: true });
 
 module.exports = mongoose.model("Beneficiaire", beneficiaireSchema);
