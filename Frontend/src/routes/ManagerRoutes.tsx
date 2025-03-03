f9c5c08a8a4e44e06e9138f82723df60000ee6b1
Boualam Fatimazohra <boualamfatima97@gmail.com>
2025-03-03 09:56:46 +0000

    Dashboard Manager frontend

--- Frontend/src/routes/ManagerRoutes.tsx
+++ Frontend/src/routes/ManagerRoutes.tsx
@@ -1,13 +1,24 @@
+import BootcampsList from "@/components/dashboardElement/Bootcamps";
+import FormateurManager from "@/components/dashboardElement/FormateurManager";
+import FormationDashboard from "@/components/dashboardElement/FormationManager";
+import { FormationProvider } from "@/contexts/FormationContext";
 import DashboardManager from "@/pages/DashboardManager";
 import { Routes, Route } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 
 const ManagerRoutes = () => {
   return (
+    <FormationProvider>
+
     <Routes>
       <Route path="/dashboardManager" element={<DashboardManager />} />
       <Route path="*" element={<NotFound />} />
+          <Route path="/FormationDashboard" element={<FormationDashboard />} /> 
+          <Route path="/FormateurManager" element={<FormateurManager />} /> 
+          <Route path="/BootcampsList" element={<BootcampsList />} />
     </Routes>
+    </FormationProvider>
+
   );
 };
 
