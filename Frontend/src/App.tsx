b9e2db51d67d2c0f8e23d6944fe9f497addb216f
salouaEng <saloua.ouissa@edu.uiz.ac.ma>
2025-03-07 12:06:06 +0100

    adaptation de ctr evenement selon les changements signalés

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -55,6 +55,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
+      <FormationProvider>
         <TooltipProvider>
           <BrowserRouter>
             <Layout>
@@ -79,7 +80,7 @@ const App = () => (
                 <Route path="/generate-link" element={<GenerateLink />} />
                 <Route path="/formulaire-evaluation" element={<FormulaireEvaluation />} />
                 <Route path="/EvaluationForm" element={<EvaluationForm />} />
-                <Route path="/BeneficiairesList" element={<BeneficiairesList />} />
+                <Route path="/BeneficiairesList" element={<BeneficiairesList/>} />
                 <Route path="/CalendarView" element={<CalendarView/>}/>
                 <Route path="/beneficiaires" element={<BeneficiairesList/>}/>
                 <Route path="/DetailsFormation" element={<DetailsFormation />} />
@@ -97,6 +98,7 @@ const App = () => (
             </Layout>
           </BrowserRouter>
         </TooltipProvider>
+        </FormationProvider>
     </AuthProvider>
   </QueryClientProvider>
 );
