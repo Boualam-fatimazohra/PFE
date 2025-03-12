0d6ece5dd8f4ffc3cba2adb41dc774ec0a086908
2tay <hakim.toutay@e-polytechnique.ma>
2025-03-12 09:32:04 +0000

    notification in dashboard header

--- Frontend/src/routes/FormateurRoutes.tsx
+++ Frontend/src/routes/FormateurRoutes.tsx
@@ -7,22 +7,18 @@ import { FormationProvider } from "@/contexts/FormationContext";
 import FormationModal from "@/components/dashboardElement/formationModal";
 import { EvenementsAssociesProvider } from '../contexts/FormateurContext';
 import { NotificationProvider } from "@/contexts/NotificationContext";
-import NotificationsHistory from "@/components/notification/NotificationsHistory";
 import EvaluationPages from "@/pages/EvaluationPages";
 import BeneficiairesList from "@/components/Formation/Beneficiaires";
 const FormateurRoutes = () => {
   return (
     <FormationProvider>
     <EvenementsAssociesProvider>
-    <NotificationProvider>
     <Routes>
       <Route path="dashboardFormateur" element={<DashboardFormateur />} />
       <Route path="mesformation" element={<MesFormations />} />
       <Route path="formationModal" element={< FormationModal/>} />
-      <Route path="notifications" element={<NotificationsHistory />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-    </NotificationProvider>
     </EvenementsAssociesProvider>
     </FormationProvider>
   );
