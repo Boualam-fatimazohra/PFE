2955fe211cd5588333c3d7cf1adf276d0f94bde1
salouaEng <saloua.ouissa@edu.uiz.ac.ma>
2025-04-07 16:39:12 +0100

    implémenter la logique de QR code coté backend

--- Backend/Routes/achat.route.js
+++ Backend/Routes/achat.route.js
@@ -3,14 +3,14 @@ const authenticated=require("../Middlewares/Authmiddleware.js");
 const authorizeRoles = require('../Middlewares/RoleMiddleware.js');
 const {createAchat,getAllAchats,getAchatById,deleteAchat,getAchatsByType} = require('../Controllers/achat.controller.js');
 
+
 const router = express.Router();
 // les routes fonctionnelles jusqu'a present 
 router.post('/createAchat',authenticated,authorizeRoles("Coordinateur"),createAchat);
 router.get('/getAllAchatsCoordinateur',authenticated,authorizeRoles("Coordinateur"),getAllAchats);
 router.get('/getAchatById/:id', authenticated,authorizeRoles("Coordinateur"),getAchatById);
-//router.put('/updateAchat',authenticated,authorizeRoles("Coordinateur"),updateAchat);
 router.delete('/deleteAchat/:id',authenticated,authorizeRoles("Coordinateur"),deleteAchat);
 router.get('/getAchatsByType/:associationType',authenticated,authorizeRoles("Coordinateur"),getAchatsByType);
-
+//router.put('/updateAchat',authenticated,authorizeRoles("Coordinateur"),updateAchat);
 
 module.exports = router;
