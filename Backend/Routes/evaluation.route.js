d1832705ca351d73fd9fc86a0ce1b08088b03ce4
Boualam Fatimazohra <boualamfatima97@gmail.com>
2025-04-15 09:14:30 +0100

    je sais pas

--- Backend/Routes/evaluation.route.js
+++ Backend/Routes/evaluation.route.js
@@ -1,12 +1,27 @@
 // Express route handler
 const express = require('express');
-const {SubmitEvaluation,sendEvaluationLinksToBeneficiaries ,createEvaluation ,} =require ('../Controllers/evaluation.controller.js');
+const {SubmitEvaluation,sendEvaluationLinksToBeneficiaries ,createEvaluation ,getAllEvaluations,deleteEvaluation,getEvaluationFormateur} =require ('../Controllers/evaluation.controller.js');
 const checkSubmission = require('../Middlewares/EvaluationMiddleware.js');
+const authorizeRoles = require('../Middlewares/RoleMiddleware.js');
+const authenticated = require('../Middlewares/Authmiddleware.js');
+const authorizeOwnership = require('../Middlewares/OwnershipMiddleware.js');
 const router = express.Router();
 // route pour enregistrer la réponse du beneficiare 
 router.post('/submitEvaluation/:token',checkSubmission,SubmitEvaluation);
 // route pour envoyer les liens de l'evaluation aux beneficiare
 router.post('/sendLinkToBeneficiare',sendEvaluationLinksToBeneficiaries);
+router.get('/getEvaluationFormateur', 
+    authenticated, 
+    authorizeRoles('Formateur'),
+    getEvaluationFormateur
+);
 
-router.post("/evaluation",createEvaluation);
+// route pour récupérer toutes les évaluations
+router.get('/AllEvaluation', getAllEvaluations);
+router.get('/deletEvaluation/:id',deleteEvaluation);
+
+router.post("/CreatEvaluation",
+    authenticated, 
+    authorizeRoles('Formateur'),
+    createEvaluation);
   module.exports = router;
